6e9e82ca094b8f43f60367867d44ed0135aa1dd6
k-hibi <k_hibi@e-savaway.jp>
2014-12-13 10:47:36 +0900

    add coloring methods

--- src/mrb_curses.c
+++ src/mrb_curses.c
@@ -118,6 +118,58 @@ mrb_curses_move(mrb_state *mrb, mrb_value self)
   return mrb_bool_value(true);
 }
 
+static mrb_value
+mrb_curses_start_color(mrb_state *mrb, mrb_value self)
+{
+  start_color();
+  return mrb_bool_value(true);
+}
+
+static mrb_value
+mrb_curses_init_pair(mrb_state *mrb, mrb_value self)
+{
+  mrb_value v1, v2, v3;
+  mrb_get_args(mrb, "iii", &v1, &v2, &v3);
+  int no = mrb_fixnum(v1);
+  int stcolor = mrb_fixnum(v2);
+  int bgcolor = mrb_fixnum(v3);
+  init_pair(no, stcolor, bgcolor);
+  return mrb_bool_value(true);
+}
+
+static mrb_value
+mrb_curses_coloron(mrb_state *mrb, mrb_value self)
+{
+  mrb_value v1;
+
+  mrb_get_args(mrb, "i", &v1);
+  int no = mrb_fixnum(v1);
+  attron(COLOR_PAIR(no));
+  return mrb_bool_value(true);
+}
+
+static mrb_value
+mrb_curses_coloroff(mrb_state *mrb, mrb_value self)
+{
+  mrb_value v1;
+
+  mrb_get_args(mrb, "i", &v1);
+  int no = mrb_fixnum(v1);
+  attroff(COLOR_PAIR(no));
+  return mrb_bool_value(true);
+}
+
+static mrb_value
+mrb_curses_wbkgd(mrb_state *mrb, mrb_value self)
+{
+  mrb_value v1;
+
+  mrb_get_args(mrb, "i", &v1);
+  int no = mrb_fixnum(v1);
+  wbkgd(stdscr, COLOR_PAIR(no));
+  return mrb_bool_value(true);
+}
+
 void mrb_mruby_curses_gem_init(mrb_state *mrb)
 {
     struct RClass *curses;
@@ -133,10 +185,24 @@ void mrb_mruby_curses_gem_init(mrb_state *mrb)
     mrb_define_class_method(mrb, curses, "addstr", mrb_curses_addstr, MRB_ARGS_REQ(1));
     mrb_define_class_method(mrb, curses, "move", mrb_curses_move, MRB_ARGS_ANY());
     mrb_define_class_method(mrb, curses, "endwin", mrb_curses_endwin, MRB_ARGS_NONE());
+    mrb_define_class_method(mrb, curses, "start_color", mrb_curses_start_color, MRB_ARGS_NONE());
+    mrb_define_class_method(mrb, curses, "init_pair", mrb_curses_init_pair, MRB_ARGS_ANY());
+    mrb_define_class_method(mrb, curses, "coloron", mrb_curses_coloron, MRB_ARGS_REQ(1));
+    mrb_define_class_method(mrb, curses, "coloroff", mrb_curses_coloroff, MRB_ARGS_REQ(1));
+    mrb_define_class_method(mrb, curses, "wbkgd", mrb_curses_wbkgd, MRB_ARGS_REQ(1));
+
+    mrb_define_const(mrb, curses, "COLOR_BLACK",  mrb_fixnum_value(COLOR_BLACK));
+    mrb_define_const(mrb, curses, "COLOR_RED",  mrb_fixnum_value(COLOR_RED));
+    mrb_define_const(mrb, curses, "COLOR_GREEN",  mrb_fixnum_value(COLOR_GREEN));
+    mrb_define_const(mrb, curses, "COLOR_YELLOW",  mrb_fixnum_value(COLOR_YELLOW));
+    mrb_define_const(mrb, curses, "COLOR_BLUE",  mrb_fixnum_value(COLOR_BLUE));
+    mrb_define_const(mrb, curses, "COLOR_MAGENTA",  mrb_fixnum_value(COLOR_MAGENTA));
+    mrb_define_const(mrb, curses, "COLOR_CYAN",  mrb_fixnum_value(COLOR_CYAN));
+    mrb_define_const(mrb, curses, "COLOR_WHITE",  mrb_fixnum_value(COLOR_WHITE));
+
     DONE;
 }
 
 void mrb_mruby_curses_gem_final(mrb_state *mrb)
 {
 }
-
